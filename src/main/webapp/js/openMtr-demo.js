e695f6748e62d5d6b8a265d52fe6855ac1f09f9c
jameswillhoite <44815350+jameswillhoite@users.noreply.github.com>
2019-04-01 10:58:34 -0400

    feat(demo-page): Added JS validation and CSS enhancements (#67)
    * chore: Add css to drag and drop
    
    * feat: Check for mobile device
    
    * feat: Added ability to remove uploaded file
    
    * fix(demo js): syntax
    
    * fix(demo JS): Changed single quote to double
    
    * fix(demo js): Codacy: Wrap anon function

--- src/main/webapp/js/openMtr-demo.js
+++ src/main/webapp/js/openMtr-demo.js
@@ -3,6 +3,19 @@ var isAdvancedUpload = (function() {
     var div = document.createElement("div");
     return (("draggable" in div) || ("ondragstart" in div && "ondrop" in div)) && "FormData" in window && "FileReader" in window;
 }());
+var mobilecheck = (function() {
+    var check = false;
+    ((function(a){
+        if(
+            /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||
+            /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))
+        )
+        {
+            check = true;
+        }
+    })(navigator.userAgent||navigator.vendor||window.opera));
+    return check;
+}());
 var openMtrDemo = {
 
     init() {
@@ -49,7 +62,7 @@ var openMtrDemo = {
         var input = form.find("input#file");
         var droppedFiles = false;
         //user allows drag n drop
-        if(isAdvancedUpload) {
+        if(isAdvancedUpload && !mobilecheck) {
             form.addClass("has-advanced-upload");
 
             form.on("drag dragstart dragend dragover dragenter dragleave drop", function(e) {
@@ -64,24 +77,44 @@ var openMtrDemo = {
                 })
                 .on("drop", function(e) {
                     self.droppedFiles = e.originalEvent.dataTransfer.files;
-                    form.addClass("upload-ready file-dropped");
-                    var reader = new FileReader();
-                    reader.onload = function(e) {
-                        form.find("div.box__fileDropped img").attr("src", e.target.result);
-                        jQuery("div#results-modal img#uploadedFile").attr("src", e.target.result);
-                    };
-                    reader.readAsDataURL(self.droppedFiles[0]);
-
-                    //For auto submit
-                    //form.trigger("submit");
+                    form.addClass("upload-ready");
+                    self.displayImg();
                 });
+            form.find("input[type='file']").on("change", function (e) {
+                self.droppedFiles = e.currentTarget.files;
+                self.displayImg();
+            });
+        }
+        else {
+            form.find("input[type='file']").on("change", function (e) {
+                self.droppedFiles = e.currentTarget.files;
+                form.addClass("has-success");
+            });
         }
 
-        //For auto submit
-        // input.on("change", function() {
-        //     form.trigger("submit");
-        // });
 
+        jQuery("div#dragNdrop span#close i").on("click", function() {
+            self.droppedFiles = null;
+            jQuery("div#dragNdrop span#close").fadeOut("fast");
+            jQuery("div#dragNdrop img").attr("src", "");
+            form.removeClass("upload-ready file-dropped");
+            jQuery("div#dragNdrop .box").fadeIn("fast");
+        });
+
+
+    },
+
+    displayImg() {
+        var self = this;
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            jQuery("div#results-modal img#uploadedFile").attr("src", e.target.result);
+            var height = jQuery("div#dragNdrop").height();
+            jQuery("div#dragNdrop .box").fadeOut("fast");
+            jQuery("div#dragNdrop img").attr("src", e.target.result).css("max-height", height);
+            jQuery("div#dragNdrop span#close").fadeIn("fast");
+        };
+        reader.readAsDataURL(self.droppedFiles[0]);
     },
 
     formListeners() {
@@ -130,6 +163,8 @@ var openMtrDemo = {
         var email = jQuery("form.my-form input#email");
         var numberOfDigits = jQuery("form.my-form input#digit-number");
 
+        self.hideErrorMsg();
+
         if(form.hasClass("is-uploading"))
         {
             return false;
@@ -138,23 +173,32 @@ var openMtrDemo = {
         var appendURL = "";
         var formData = new FormData();
 
-        //If there is a file, then upload the file
-        if(isAdvancedUpload && self.droppedFiles) {
+        if(!self.droppedFiles && url.val().length === 0) {
+            self.displayErrorMsg("Error", "Please submit an image to read.");
+            return false;
+        }
+        else if(self.droppedFiles) {
             //Possibly upload more than one file
             jQuery.each(self.droppedFiles, function(i, file){
                 formData.append("file", file);
             });
         }
         else  {
-            //ToDo: Add url validation
+            if(!self.validateUrl(url.val())) {
+                self.displayErrorMsg("Error", "The URL address is invalid.");
+                return false;
+            }
             formData.append("url", url.val());
             appendURL = "/url";
         }
 
 
         if(self.validateEmail(email.val())) {
-            //ToDo: Add email validation
             formData.append("email", email.val());
+        }
+        else {
+            self.displayErrorMsg("Error", "The E-mail address is invalid.");
+            return false;
         }
 
         if(numberOfDigits.val()) {
@@ -219,10 +263,12 @@ var openMtrDemo = {
         var numberOfDigits = jQuery("form.my-form input#digit-number");
 
         form.find("div.box__fileDropped img").attr("src", "");
-        form.removeClass("upload-ready file-dropped");
+        form.removeClass("upload-ready has-success is-error");
         numberOfDigits.val("");
         numberOfDigits.parent().find("div.text").html("Select").addClass("default");
         self.droppedFiles = null;
+
+        url.val("");
 
 
     },
